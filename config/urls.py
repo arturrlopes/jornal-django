99b90fabb4165b6258b95c569ad9dfc8dff8b48f
artur lopes <artur.lopes.2025033@my.istec.pt>
2026-04-14 18:03:22 -0300

    Corrigir migrações e finalizar projeto

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('noticias.urls')),   # <-- adicionar esta linha
+    path('', include('noticias.urls')),
 ]
