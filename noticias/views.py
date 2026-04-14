001ef8dd85b91789548dd24e0b7df1745c9a7e8f
artur lopes <artur.lopes.2025033@my.istec.pt>
2026-04-13 21:08:37 -0300

    Implementar views e URLs para lista, detalhe e comentários

--- noticias/views.py
+++ noticias/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Artigo, Comentario
 
-# Create your views here.
+def lista_artigos(request):
+    artigos = Artigo.objects.all().order_by('-data_publicacao')
+    return render(request, 'noticias/lista.html', {'artigos': artigos})
+
+def detalhe_artigo(request, artigo_id):
+    artigo = get_object_or_404(Artigo, pk=artigo_id)
+    return render(request, 'noticias/detalhe.html', {'artigo': artigo})
+
+def comentarios_artigo(request, artigo_id):
+    artigo = get_object_or_404(Artigo, pk=artigo_id)
+    if request.method == 'POST':
+        texto = request.POST.get('texto')
+        if texto:
+            Comentario.objects.create(artigo=artigo, texto=texto)
+            return redirect('comentarios_artigo', artigo_id=artigo.id)
+    comentarios = artigo.comentarios.all().order_by('-data_publicacao')
+    return render(request, 'noticias/comentarios.html', {
+        'artigo': artigo,
+        'comentarios': comentarios
+    })
